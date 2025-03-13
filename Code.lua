b3108a03607067c95661e573c9b41fa2b9ab2389
Guhttanin <naoseimds095@gmail.com>
2025-03-13 09:35:07 -0300

    Update Code.lua

--- Code.lua
+++ Code.lua
@@ -1,32 +1,5 @@
-local Fluent = loadstring(game:HttpGet("https://github.com/ActualMasterOogway/Fluent-Renewed/releases/latest/download/Fluent.luau"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
-if Fluent then
-    local player = game.Players.LocalPlayer
-    local character = player.Character or player.CharacterAdded:Wait()
-    local humanoid = character:WaitForChild("Humanoid")
-    
-    local Window = Fluent:CreateWindow({
-        Title = "True Love Cheats - Color or Die",
-        SubTitle = "Desenvolvido por Guhttanin",
-        Size = UDim2.fromOffset(720, 400),
-        Theme = "Aqua",
-    })
-
-    local Auto_Win_Tab = Window:AddTab({ Title = "Auto Win", Icon = "settings" })
-
-    local Auto_Win_Tab_Button = Auto_Win_Tab:AddButton({
-            Title = "Auto Win",
-            Callback = function()
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/GuhttaninHub/Auto-Zerar-Color_Or_Die/refs/heads/main/Auto_Zerar.lua"))()
-            end
-    })
-
-    local Teleport_Tab = Window:AddTab({ Title = "Teleport", Icon = "settings" })
-
-    local Teleport_Tab_Dropdown = Teleport_Tab:Dropdown
-    
-    Window:Show()
-else
-    warn("Erro ao carregar o Fluent.")
-end
-
+Section:NewDropdown("DropdownText", "DropdownInf", {"Option 1", "Option 2", "Option 3"}, function(currentOption)
+    print(currentOption)
+end)
