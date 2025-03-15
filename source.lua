5a987a635b3e5ea127fd5b825e94e09940ecdf4d
Guhttanin <naoseimds095@gmail.com>
2025-03-15 09:55:23 -0300

    Update source.lua

--- source.lua
+++ source.lua
@@ -2,7 +2,7 @@ local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/realr
 
 local Window = redzlib:MakeWindow({
     Title = "Ghost-X Hub",
-    SubTitle = "Made by Guhttanin",
+    SubTitle = "Desenvolvido por Guhttanin",
     SaveFolder = "Redz Config"
 })
 
@@ -17,6 +17,7 @@ Window:AddMinimizeButton({
 })
 
 local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
 
 local Auto_Win_Tab = Window:MakeTab({ "Auto Win", "Auto Win" })
 
@@ -47,4 +48,42 @@ end})
 
 Chapters_Tab:AddButton({"Teletransport Chapter 4", function()
     TP:Teleport(Chapter4, player)
+end})
+
+local Secrets_Rooms_Tab = Window:MakeTab({"Secrets Rooms", "Secrets Rooms"})
+
+HammerRoom = 215.925003, 15.3999958, 168.499985, 0, 0, 1, 0, 1, -0, -1, 0, 0
+
+KeyRoom = 258.270081, 12.3500004, -182.982941, 0, 0, -1, 0, 1, 0, 1, 0, 0
+
+PlankRoom = 44.9749908, 40.9499969, -15.4500046, 0, 0, 1, 0, 1, -0, -1, 0, 0
+
+PuzzleRoom = 133.711212, 11.3930454, -133.120468, 0, 0, 1, 0, 1, -0, -1, 0, 0
+
+SawRoom = 151.25, 36.6930466, -6.10000038, 1, 0, 0, 0, 1, 0, 0, 0, 1
+
+ScrewDriverRoom = 390.170105, 11.1000004, 29.8170528, 1, 0, 0, 0, 1, 0, 0, 0, 1
+
+Secrets_Rooms_Tab:AddButton({"Teletransport Hammer Secret Room", function() 
+    character:SetPrimaryPartCFrame(CFrame.new(HammerRoom))
+end})
+
+Secrets_Rooms_Tab:AddButton({"Teletransport Key Secret Room", function() 
+    character:SetPrimaryPartCFrame(CFrame.new(KeyRoom))
+end})
+
+Secrets_Rooms_Tab:AddButton({"Teletransport Plank Secret Room", function() 
+    character:SetPrimaryPartCFrame(CFrame.new(PlankRoom))
+end})
+
+Secrets_Rooms_Tab:AddButton({"Teletransport Puzzle Secret Room", function() 
+    character:SetPrimaryPartCFrame(CFrame.new(PuzzleRoom))
+end})
+
+Secrets_Rooms_Tab:AddButton({"Teletransport Saw Secret Room", function() 
+    character:SetPrimaryPartCFrame(CFrame.new(SawRoom))
+end})
+
+Secrets_Rooms_Tab:AddButton({"Teletransport ScrewDriver Secret Room", function() 
+    character:SetPrimaryPartCFrame(CFrame.new(ScrewDriverRoom))
 end})
